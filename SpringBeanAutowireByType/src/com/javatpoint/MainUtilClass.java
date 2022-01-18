39d9a08554b0fca66fab5d2c59c3351e3fd93ee9
ChaitanyaWarad <94101135+ChaitanyaWarad@users.noreply.github.com>
2022-01-18 16:10:23 +0530

    Add files via upload

---
+++ SpringBeanAutowireByType/src/com/javatpoint/MainUtilClass.java
@@ -0,0 +1,17 @@
+package com.javatpoint;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainUtilClass {
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("springconfig.xml");
+		Employee emp = (Employee) context.getBean("emp");
+		System.out.println(emp.getId());
+		System.out.println(emp.getName());
+		System.out.println();
+		System.out.println(emp.getAddress().getAddressLine1());
+		System.out.println(emp.getAddress().getCity());
+		System.out.println(emp.getAddress().getState());
+		System.out.println(emp.getAddress().getCountry());
+	}
+}
