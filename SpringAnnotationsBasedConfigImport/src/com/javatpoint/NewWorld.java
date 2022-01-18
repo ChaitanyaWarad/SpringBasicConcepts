fed9036a16a9919428879c46e8856f216d45831b
ChaitanyaWarad <94101135+ChaitanyaWarad@users.noreply.github.com>
2022-01-18 16:08:32 +0530

    Add files via upload

---
+++ SpringAnnotationsBasedConfigImport/src/com/javatpoint/NewWorld.java
@@ -0,0 +1,15 @@
+package com.javatpoint;
+
+public class NewWorld {
+
+	private String newMessage;
+
+	public String getNewMessage() {
+		return newMessage;
+	}
+
+	public void setNewMessage(String newMessage) {
+		this.newMessage = newMessage;
+	}
+
+}
