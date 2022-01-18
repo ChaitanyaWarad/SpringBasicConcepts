8a27d6cae0c1b0b25093838e2e4c2a932e54ef85
ChaitanyaWarad <94101135+ChaitanyaWarad@users.noreply.github.com>
2022-01-18 16:19:29 +0530

    Add files via upload

---
+++ SpringFactoryMethod/src/com/javatpoint/EmployeeDao.java
@@ -0,0 +1,18 @@
+package com.javatpoint;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class EmployeeDao {
+
+	public JdbcTemplate jdbcTemplate;
+
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+	public int saveEmployee(Employee e) {
+		String query = "insert into employee values('" + e.getId() + "','" + e.getName() + "','" + e.getSalary() + "')";
+		return jdbcTemplate.update(query);
+	}
+
+}
